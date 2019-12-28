8a58136d8926372bda8ac55a5792ce33a582b587
Ozna-work <PuissanceBinaire1910>
2019-12-28 15:36:07 +0100

    VERSION FINALE

--- js/googleMap.js
+++ js/googleMap.js
@@ -19,11 +19,17 @@ var greenIcon = L.icon({
 var markerSainteCroix = L.marker([48.772, 6.896], {icon: greenIcon}).addTo(mymap).on('click', onClick); // On ajoute un event au pin qui est notre zoo le plus grand
 var markerPalmyre = L.marker([45.686, -1.165], {icon: greenIcon}).addTo(mymap);
 var markerPeaugres = L.marker([45.266, 4.723], {icon: greenIcon}).addTo(mymap);
+var markerAmneville = L.marker([49.246, 6.140], {icon: greenIcon}).addTo(mymap);
+var markerThoiry = L.marker([48.864, 1.796], {icon: greenIcon}).addTo(mymap);
+var markerFleche = L.marker([47.677, -0.047], {icon: greenIcon}).addTo(mymap);
 
 //On ajoute un popup à chaque pin
 markerSainteCroix.bindPopup("Zoo de Sainte Croix, plus grand zoo de France !")
 markerPalmyre.bindPopup("Zoo de Palmyre !");
 markerPeaugres.bindPopup("Zoo de Peaugres !");
+markerAmneville.bindPopup("Zoo d'Amnéville !");
+markerThoiry.bindPopup("Zoo de Thoiry !");
+markerFleche.bindPopup("Zoo de La Flèche !");
 
 //Permet la mise en place d'openStreetMap, auquel on ajoute notre map précédément crée
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -47,4 +53,25 @@ markerPeaugres.on('click',function () {
 });
 mymap.on('click', function () {
     mymap.removeLayer(circle);
+});
+
+//Pour gérer l'apparence des flêches des moyens de contacts
+$(document).ready(function () {
+    $('.collapse')
+        .on('shown.bs.collapse', function(e) {
+            let str = e.target.id.replace('info','');   // Afin d'avoir l'id de la div où il y a la flêche
+            let elem = $("#"+str);                      // On récupère le div
+            elem.find(".fa-chevron-down")               // On trouve l'élément ayant pour classe la flêche vers le bas
+                .removeClass("fa-chevron-down")      // On supprime cette classe
+                .addClass("fa-chevron-up");          // On ajoute la classe bootstrap pour avoir l'icône de flèche vers le haut
+        })
+
+        // Semblable à ce qu'il y a précedement mais pour le cas où on souhaite cacher des informations
+        .on('hidden.bs.collapse', function(e) {
+            let str = e.target.id.replace('info','');
+            let elem = $("#"+str);
+            elem.find(".fa-chevron-up")
+                .removeClass("fa-chevron-up")
+                .addClass("fa-chevron-down");
+        });
 });
